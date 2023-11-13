239a1956f304c0288673e6831185ad60a3b89f43
Karthik Krishnan K K <110273701+KarthikKKrishnan@users.noreply.github.com>
2023-11-14 00:12:13 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,14 @@
+let movies = [
+    {
+        image: 'img/banner/banner1.jpg'
+    },
+    {
+        image: 'img/banner/banner2.jpq'
+    },
+    {
+        image: 'img/banner/banner3.jpg'
+    },
+    {
+        image: 'img/banner/banner4.jpg'
+    },
+]
